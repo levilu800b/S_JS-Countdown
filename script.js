f627b6d2d408efe5658f97e7f6e61a2147d73911
levilu800b <levilu800b@gmail.com>
2021-06-13 14:59:20 +0100

    save

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-	const countDate = new Date('June 10, 2021 00:00:00').getTime();
+	const countDate = new Date('December 1, 2021 00:00:00').getTime();
 	const now = new Date().getTime();
 	const gap = countDate - now;
 
