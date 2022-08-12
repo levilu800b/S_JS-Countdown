8e619124c2b29ea4fc4f543f75e6a07b36bc435d
levilu800b <levilu800b@gmail.com>
2022-08-12 14:16:24 +0100

    Date change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-	const countDate = new Date('July 1, 2022 00:00:00').getTime();
+	const countDate = new Date('December 25, 2022 00:00:00').getTime();
 	const now = new Date().getTime();
 	const gap = countDate - now;
 
